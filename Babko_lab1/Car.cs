e9450e4ed49dd059e5e4c21471eab47fd9770e2f
mykola-b31 <babkokolya81@gmail.com>
2026-02-03 18:48:14 +0200

    initial commit: lab 1 implementation

---
+++ Babko_lab1/Car.cs
@@ -0,0 +1,26 @@
+﻿namespace ua.cn.stu;
+
+public class Car
+{
+    public string LicensePlate { get; set; }
+    private string Brand { get; set; }
+    private string Model { get; set; }
+    private int Year { get; set; }
+    private double EngineVolume { get; set; }
+    private bool IsAutomatic { get; set; }
+
+    public Car(string licensePlate, string brand, string model, int year, double engineVolume, bool isAutomatic)
+    {
+        LicensePlate = licensePlate;
+        Brand = brand;
+        Model = model;
+        Year = year;
+        EngineVolume = engineVolume;
+        IsAutomatic = isAutomatic;
+    }
+
+    public override string ToString()
+    {
+        return $"[{LicensePlate}] {Brand} {Model} ({Year}), {EngineVolume}L, Auto: {IsAutomatic}";
+    }
+}
---
+++ Babko_lab1/Program.cs
@@ -0,0 +1,82 @@
+﻿namespace ua.cn.stu;
+
+class Program
+{
+    private static IDictionary<string, Car> _carDictionary = new Dictionary<string, Car>();
+    private static void Main(string[] args)
+    {
+        for (;;)
+        {
+            Console.WriteLine("Please enter command add, remove, find, list or exit: ");
+            var command = Console.ReadLine();
+            switch (command)
+            {
+                case "add":
+                {
+                    Console.WriteLine("Enter license plate:");
+                    var licensePlate = Console.ReadLine() ?? "";
+                    if (_carDictionary.ContainsKey(licensePlate) || licensePlate == "")
+                    {
+                        Console.WriteLine("Error: Car with this license plate already exists!");
+                        break;
+                    }
+                    Console.WriteLine("Enter brand:");
+                    var brand = Console.ReadLine() ?? "";
+                    Console.WriteLine("Enter model: ");
+                    var model = Console.ReadLine() ?? "";
+                    Console.WriteLine("Enter year:");
+                    int.TryParse(Console.ReadLine(), out var year);
+                    Console.WriteLine("Enter engine volume:");
+                    double.TryParse(Console.ReadLine(), out var engineVolume);
+                    Console.WriteLine("Is automatic transmission? (true/false):");
+                    bool.TryParse(Console.ReadLine(), out var isAutomatic);
+                    
+                    var car = new Car(licensePlate, brand, model, year, engineVolume, isAutomatic);
+                    _carDictionary.Add(car.LicensePlate, car);
+                    Console.WriteLine("Car was successfully added to collection");
+                    break;
+                }
+                case "remove":
+                {
+                    Console.WriteLine("Enter license plate:");
+                    var licensePlate = Console.ReadLine();
+                    if (_carDictionary.Remove(licensePlate!))
+                    {
+                        Console.WriteLine("Car was removed from collection");
+                    }
+                    else 
+                    {
+                        Console.WriteLine("Error: Car not found.");
+                    }
+                    break;
+                }
+                case "list":
+                {
+                    foreach (var car in _carDictionary.Values)
+                    {
+                        Console.WriteLine(car.ToString());
+                    }
+
+                    break;
+                }
+                case "find":
+                {
+                    Console.WriteLine("Enter license plate to search:");
+                    var searchKey = Console.ReadLine() ?? "";
+                    if (_carDictionary.TryGetValue(searchKey, out var foundCar)) 
+                    {
+                        Console.WriteLine(foundCar.ToString());
+                    }
+                    else
+                    {
+                        Console.WriteLine("Car not found.");
+                    }
+                    break;
+                }
+                case "exit":
+                    Console.WriteLine("Exiting the program.");
+                    return;
+            } 
+        }
+    }
+}
